c1eaea961cd37765c4f720f3d7230094c66f9b3d
parkjiyun <jyun38@gmail.com>
2014-09-18 15:35:40 +0900

    sergser

---
+++ code.js
@@ -0,0 +1,26 @@
+function setup() {
+	createCanvas(640, 480);
+
+	backgroundColor = color(0, 0, 0);
+	fillColor = color(0, 0, 255)
+}
+
+function draw() {
+	background(backgroundColor);
+
+	fillColor = color(Math.random() * 255,
+		Math.random() * 255,
+		Math.random() * 255.
+		Math.random() * 255);
+
+	mouseColor = color(mouseX / 640 * 255,
+		mouseY / 480 * 255,
+		0
+		);
+
+	// fill(fillColor);
+	// rect(100, 100, 100, 100);
+
+	fill(mouseColor);
+	ellipse(300, 300, 100, 100);
+}
