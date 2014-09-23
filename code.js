f0dc22cfd5742b01d750c86d4254856b2c682509
parkjiyun <jyun38@gmail.com>
2014-09-23 13:31:19 +0900

    awefwef

--- code.js
+++ code.js
@@ -1,31 +1,26 @@
-backgroundColor = color(0, 0, 0, 255);
-background(backgroundColor);
-
-fillColor = color(255, 0, 0, 128);
-fill(fillColor);
-randomColor!!
-
-randomColor = color(
-  Math.random() * 255,
-  Math.random() * 255,
-  Math.random() * 255,
-  Math.random() * 255);
-mouseColor
-
-canvasWidth = 640;
-canvasHeight = 480;
-
 function setup() {
-  createCanvas(canvasWidth, canvasHeight);
+   createCanvas(640, 400);
+   
+   backgroundColor = color(0, 0, 255);
+   fillColor = color(0, 0, 255);
 }
 
 function draw() {
-  mouseColor = color(
-    mouseX / canvasWidth * 255,
-    mouseY / canvasWidth * 255,
-    0,
-    0);
+   background(backgroundColor);
+   
+   fillColor = color(
+      Math. random() *255,
+      Math. random() *255, 
+      Math. random() *255, 
+      Math. random() *255);
 
-  fill(mouseColor);
-  // 이 밑에 그리기
+   mouseColor = color(
+      mouseX / 640 * 128 + Math. random() * 128,
+      mouseY / 480 * 255, 0);
+
+// fill(fillColor);
+// rect(100, 100, 100, 100);
+
+   fill(fillColor);
+   rect(100, 100, 100, 100);
 }
